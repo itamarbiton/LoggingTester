b5ec835be352e51a57d0cb5a82bebebf8613b00f
Itamar <bitonitamar@gmail.com>
2018-12-11 09:25:23 +0200

    Initial Commit

---
+++ LoggingTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoggingTester
+//
+//  Created by Itamar Biton on 11/12/2018.
+//  Copyright © 2018 Itamar Biton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
